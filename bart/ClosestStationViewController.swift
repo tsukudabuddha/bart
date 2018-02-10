6e4c5fb2da5142e9ac7a54c7d849a39fd5b0965a
tsukudabuddha <andrewtsukuda@gmail.com>
2018-02-10 08:24:22 -0800

    Added current station vc

---
+++ bart/ClosestStationViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ClosestStationViewController.swift
+//  bart
+//
+//  Created by Andrew Tsukuda on 2/9/18.
+//  Copyright © 2018 Andrew Tsukuda. All rights reserved.
+//
+
+import UIKit
+
+class ClosestStationViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        /* Make tableview transparent */
+//        self.tableView.layer.backgroundColor = 
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        return cell
+    
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
