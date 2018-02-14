b3676edff18a66b287ebb909bf97439a0cb099c1
tsukudabuddha <andrewtsukuda@gmail.com>
2018-02-14 11:18:43 -0800

    Changed tab bar style to black and now loads current station first

---
+++ bart/View Controllers/MainTabBarViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MainTabBarViewController.swift
+//  bart
+//
+//  Created by Andrew Tsukuda on 2/14/18.
+//  Copyright © 2018 Andrew Tsukuda. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.selectedIndex = 1 // Load current station view first
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
