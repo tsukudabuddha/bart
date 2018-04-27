6966cabd3bfa93b69d47e71310c2b326dd8d1b37
tsukudabuddha <andrewtsukuda@gmail.com>
2018-04-27 14:44:42 -0700

    added pop up xib

---
+++ bart/View Controllers/DirectionsPopupViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DirectionsPopupViewController.swift
+//  bart
+//
+//  Created by Andrew Tsukuda on 4/27/18.
+//  Copyright © 2018 Andrew Tsukuda. All rights reserved.
+//
+
+import UIKit
+
+class DirectionsPopupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
